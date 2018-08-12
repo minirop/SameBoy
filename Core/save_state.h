2f9ca88c87c9be04a248d00219a07ca6b9a37416
minirop <minirop@hotmail.com>
2018-08-12 18:21:17 +0200

    remove warning about double semicolon

--- Core/save_state.h
+++ Core/save_state.h
@@ -5,7 +5,7 @@
 
 #define GB_PADDING(type, old_usage) type old_usage##__do_not_use
 
-#define GB_SECTION(name, ...) __attribute__ ((aligned (8))) struct { char c; } name##_section_start; __VA_ARGS__; struct {char c; } name##_section_end
+#define GB_SECTION(name, ...) __attribute__ ((aligned (8))) struct { char c; } name##_section_start; __VA_ARGS__ struct { char c; } name##_section_end
 #define GB_SECTION_OFFSET(name) (offsetof(GB_gameboy_t, name##_section_start))
 #define GB_SECTION_SIZE(name) (offsetof(GB_gameboy_t, name##_section_end) - offsetof(GB_gameboy_t, name##_section_start))
 #define GB_GET_SECTION(gb, name) ((void*)&((gb)->name##_section_start))
